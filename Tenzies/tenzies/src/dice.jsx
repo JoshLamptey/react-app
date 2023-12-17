874393a224046cc3086afb744c3cc0e1839a1b0d
JoshLamptey <107364497+JoshLamptey@users.noreply.github.com>
2023-12-17 00:11:18 -1200

    done

--- Tenzies/tenzies/src/dice.jsx
+++ Tenzies/tenzies/src/dice.jsx
@@ -3,9 +3,14 @@ export default function Die(prop){
     const styles = {
         backgroundColor : prop.held ? "#59E391" : "white"
     }
+
     return (
-        <div className="die" style={styles}>
-            <h2 className="die-num">{prop.value}</h2>
+        <div
+         className="die"
+          style={styles} 
+          onClick={prop.holdDice}
+          >
+            <h2 className="die-num" >{prop.value}</h2>
         </div>
     )
 }
